915729bdae392fdcd097698b32cf05d0bc50ef0d
HunFuzeMain <hunfuzemain@gmail.com>
2025-03-21 08:06:36 +0100

    Delete Package.cs

--- Package.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VizsgaremekApp.Models
-{
-    public class Package
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
