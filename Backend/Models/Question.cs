e232d2ef9ffd4fcd9cc9e62a5a291a7a7d8cd2a8
HunFuzeMain <hunfuzemain@gmail.com>
2025-05-04 11:35:55 +0200

    Update Question.cs

--- Backend/Models/Question.cs
+++ Backend/Models/Question.cs
@@ -4,5 +4,6 @@ public class Question
 {
     public int Id { get; set; }
     public string Text { get; set; } = string.Empty;
+    public string Email {get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
